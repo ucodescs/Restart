10a358f05ad80e03bccf8823a568e7b684557ff6
Renato Ribeiro <renato.ribeiro@ucode.pt>
2022-03-29 22:36:40 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Renato Ribeiro on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
